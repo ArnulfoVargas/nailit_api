5e6db1438a23ccf5e14fc7b484f93efbae9a1221
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2024-10-31 19:20:56 -0600

    Update handle_controllers.go

--- cmd/handle_controllers.go
+++ cmd/handle_controllers.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/ArnulfoVargas/nailit_api.git/cmd/controllers"
+import (
+	"github.com/ArnulfoVargas/nailit_api.git/cmd/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func (server *Server) handleControllers() {
 	userController := controllers.NewUserController(server.db)
@@ -8,4 +11,9 @@ func (server *Server) handleControllers() {
 	userGroup := server.app.Group("/user")
 
 	userGroup.Post("/login", userController.Login)
+	server.app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"hello": "world",
+		})
+	})
 }
